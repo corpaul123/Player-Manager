92db764eb03250a92aa6fd8c179874ec97b7df30
corpaul123 <111656005+corpaul123@users.noreply.github.com>
2025-08-28 06:14:19 +0000

    commit

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import java.lang.*;
 
 public class Main{
 
-  public static void EnterEnemy(int enemyInit, int enemyCount, HashMap<String, Integer> initiative){
+  public static void addEnemy(int enemyInit, int enemyCount, HashMap<String, Integer> initiative){
 
     int ini = enemyInit;
     for(int i = 0; i < enemyCount; i++){
@@ -15,6 +15,43 @@ public class Main{
 
 
   }
+
+
+
+
+
+
+  //repeat for number of entities to retrieve names and initiative rolls
+  public static HashMap<String, Integer> EnterInfo(int entCount){
+    HashMap<String, Integer> ents = new HashMap<String, Integer>();
+    int cont = entCount;
+    while(cont > 0){
+
+      System.out.println("input name and initiative:" );
+      Scanner scan = new Scanner(System.in);
+      String name = scan.nextLine();
+      int init = scan.nextInt();
+      scan.nextLine();
+      //System.out.println("name: " + name + " initiative: " + init);
+      if(init > 0 && init <= 40){
+        ents.put(name, init);
+        cont--;
+      }
+      else{
+        System.out.println("Invalid initiative, must be between 0 and 40");
+        continue;
+      }
+
+    }
+
+
+    return ents;
+  }
+
+
+
+
+
 
   //method sorts the map
   public static void sort(HashMap<String, Integer> initiative){
@@ -45,6 +82,12 @@ public class Main{
 
 
   }
+
+
+
+
+
+
   public static void main(String[] args) {
     //create hashmap
     HashMap<String, Integer> initiative = new HashMap<String, Integer>();
@@ -57,23 +100,9 @@ public class Main{
     System.out.print("Enter number of entities (all enemies will be a single entity): ");
     int entCount = scanner.nextInt();
     scanner.nextLine();
-
+    initiative = EnterInfo(entCount);
+    
     //System.out.println(entCount);
-
-
-
-    //repeat for number of entities to retrieve names and initiative rolls
-    for(int i = 0; i < entCount; i++){
-
-      System.out.println("input name and initiative:" );
-      String name = scanner.nextLine();
-      int init = scanner.nextInt();
-      scanner.nextLine();
-      //System.out.println("name: " + name + " initiative: " + init);
-      initiative.put(name, init);
-
-      
-    }
     int check = 0;
     while(check == 0){
       System.out.println("Are there enemies in this encounter? Y/N ");
@@ -94,13 +123,24 @@ public class Main{
     }
 
     if(enem == true){
-      System.out.println("add enemy amount and initiative: ");
-      int enemyCount = scanner.nextInt();
-      scanner.nextLine();
-      int enemyInit = scanner.nextInt();
-      scanner.nextLine();
-
-      EnterEnemy(enemyInit,enemyCount, initiative);
+      int enemCheck = 1;
+      int enemyCount = 0;
+      int enemyInit = 0;
+      while(enemCheck == 1){
+          
+        System.out.println("add enemy amount and initiative: ");
+        enemyCount = scanner.nextInt();
+        scanner.nextLine();
+        enemyInit = scanner.nextInt();
+        scanner.nextLine();
+        if((enemyCount > 0 && enemyInit > 0) && enemyInit <= 40){
+          enemCheck = 0;
+        }
+        else{
+          System.out.println("Inivalid enemy information, please re-enter");
+        }
+      }
+      addEnemy(enemyInit,enemyCount, initiative);
 
     }
     else{
